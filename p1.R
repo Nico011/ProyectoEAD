475642056577931dedcdd59eb83fe0870b538d2e
Nicolás H <nicolas.hervias@live.com>
2020-07-02 02:36:20 -0400

    lista la tabla y el gráfico Confirmados-Fallecidos

--- p1.R
+++ p1.R
@@ -19,14 +19,14 @@ str(maule.activos)
 # maule.colnames <- colnames(maule.activos)[6:ncol(maule.activos)]
 # maule.colnames
 
-ggplot(data = maule.activos, aes(x=Comuna, y=colnames(maule.activos)[ncol(maule.activos)])) +
-  geom_bar(stat = "identity", width = 0.5) +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+#ggplot(data = maule.activos, aes(x=Comuna, y=colnames(maule.activos)[ncol(maule.activos)])) +
+#  geom_bar(stat = "identity", width = 0.5) +
+#  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
 
 
-ggplot(data = maule.activos, aes(x=c(colnames(maule.activos)[6:ncol(maule.activos)]), y=Region)) +
-  geom_bar(stat = "identity", width = 0.5) +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+# ggplot(data = maule.activos, aes(x=c(colnames(maule.activos)[6:ncol(maule.activos)]), y=Region)) +
+#   geom_bar(stat = "identity", width = 0.5) +
+#   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
 
 
 # Casos fallecidos ######################################
@@ -65,5 +65,58 @@ tabla <- cbind(tabla.cols1,tabla.cols2, tabla.cols3, tabla.cols4)
 
 names(tabla) <- c("Comuna", "Poblacion", "Confirmados", "Incremento", "Fallecidos", "Casos.100k")
 row.names(tabla) <- c(1:nrow(tabla))
+tabla
+t.tabla <- t(tabla)
+t.tabla.colnames <- t.tabla[1,]
+as.factor(t.tabla.colnames)
+t.tabla <- t.tabla[2:nrow(t.tabla),]
+colnames(t.tabla) <- t.tabla.colnames # se asignan las comunas como nombre de las columnas
+
+t.tabla
+
+# ggplot(data = tabla, aes(fill = Fallecidos, y = Confirmados, x = Comuna)) +
+#   geom_bar(position = "stack", stat = "identity", width = 0.5) +
+#   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
 
 
+
+# intento 1
+ggplot(data = tabla) +
+  geom_bar(mapping = aes(x = Comuna, fill = Fallecidos)) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+
+#intento 2
+ggplot(data = tabla) +
+  geom_bar(mapping = aes(x = Comuna, fill = Confirmados), position = "fill") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+
+tabla
+
+# intento 3
+ggplot(data = tabla, aes(x = Comuna, y = Confirmados, fill = Fallecidos)) +
+  geom_bar(position = "stack", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+
+# intento 4
+t1<- tabla[1:(nrow(tabla)-1),] # le quitamos la fila con el total
+
+ggplot(data=t1, aes(x = Comuna, y = Confirmados, fill = Fallecidos)) +
+  geom_bar(stat = "identity")+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+
+# internto 5 $$$$$
+ggplot(data = t1, aes(x = Comuna, y = Confirmados, color = as.factor(Fallecidos))) +
+  geom_bar(stat = "identity", position = "dodge") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+
+#t.tabla2 <- as.data.frame(t.tabla)
+
+# intento 6 con facetas
+ggplot(data = t1, aes(x = Comuna, y = Confirmados, fill = Fallecidos)) +
+  geom_bar(position = "dodge", stat = "identity") +
+  facet_wrap(~Fallecidos)+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+
+# ggplot ####################################
+ggplot(data = t1, aes(x = Comuna, y = Confirmados, color = Fallecidos)) +
+  geom_line()
