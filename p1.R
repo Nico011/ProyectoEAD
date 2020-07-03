3432c0769b367e52a261393c35b595b5f4337cb3
Nicolás H <nicolas.hervias@live.com>
2020-07-03 05:03:15 -0400

    lista la tabla y casos por región

--- p1.R
+++ p1.R
@@ -13,7 +13,7 @@ raw.activos <- read.csv(url("https://raw.githubusercontent.com/MinCiencia/Datos-
 
 # se extraen los datos de la región del Maule
 maule.activos <- raw.activos[raw.activos$Region == "Maule",]
-str(maule.activos)
+#str(maule.activos)
 
 # se obtienen los nombres de las columnas que son fechas
 # maule.colnames <- colnames(maule.activos)[6:ncol(maule.activos)]
@@ -34,19 +34,18 @@ str(maule.activos)
 raw.fallec <- read.csv("https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/input/InformeEpidemiologico/CasosFallecidosPorComuna.csv")
 maule.fallec <- raw.fallec[raw.fallec$Region == "Maule",]
 maule.fallec.simple <- maule.fallec[c(3, 5, ncol(maule.fallec))]
-maule.fallec.simple
 
 # Casos acumulados ######################################
 # fuente de datos: https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/input/InformeEpidemiologico/CasosAcumuladosPorComuna.csv
 raw.acum <- read.csv("https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/input/InformeEpidemiologico/CasosAcumuladosPorComuna.csv")
 maule.acum <- raw.acum[raw.acum$Region == "Maule",]
 
-vals <- data.frame("Maule", 7, "Total", "")
-names(vals) <- c("Region", "Codigo.region", "Comuna", "Codigo.comuna")
-totales <- mapply(sum, maule.acum[,5:ncol(maule.acum)], na.rm = TRUE)
-row.totales <- c(vals, totales)
-maule.acum.tot <- rbind(maule.acum, row.totales) # agregada la fila con totales
-
+#vals <- data.frame("Maule", 7, "Total", "")
+#names(vals) <- c("Region", "Codigo.region", "Comuna", "Codigo.comuna")
+#totales <- mapply(sum, maule.acum[,5:ncol(maule.acum)], na.rm = TRUE)
+#row.totales <- c(vals, totales)
+#maule.acum.tot <- rbind(maule.acum, row.totales) # agregada la fila con totales
+#se quitó maule.acum.total porque ahora si tiene una fila para los totales
 
 
 
@@ -54,12 +53,12 @@ maule.acum.tot <- rbind(maule.acum, row.totales) # agregada la fila con totales
 # para la tabla son necesarias la columna Comuna (3), Población (5), Confirmados (a la última fecha), 
 # incremento (dif ultima fecha - fecha anterior) tasa (última fila), que corresponde a
 # los confirmados/100k habitantes
-ultima.col.acum <- ncol(maule.acum.tot)-1
-tabla.cols1 <- maule.acum.tot[,c(3, 5, ultima.col.acum)]
+ultima.col.acum <- ncol(maule.acum)-1
+tabla.cols1 <- maule.acum[,c(3, 5, ultima.col.acum)]
 # calcular el aumento usando la penúltima columna y la anterior
-tabla.cols2 <- maule.acum.tot[,ultima.col.acum] - maule.acum.tot[,ultima.col.acum-1]
+tabla.cols2 <- maule.acum[,ultima.col.acum] - maule.acum[,ultima.col.acum-1]
 tabla.cols3 <- maule.fallec.simple[,ncol(maule.fallec.simple)]
-tabla.cols4 <- maule.acum.tot[, ncol(maule.acum.tot)]
+tabla.cols4 <- maule.acum[, ncol(maule.acum)]
 
 tabla <- cbind(tabla.cols1,tabla.cols2, tabla.cols3, tabla.cols4)
 
@@ -74,48 +73,67 @@ colnames(t.tabla) <- t.tabla.colnames # se asignan las comunas como nombre de la
 
 t.tabla
 
-# ggplot(data = tabla, aes(fill = Fallecidos, y = Confirmados, x = Comuna)) +
-#   geom_bar(position = "stack", stat = "identity", width = 0.5) +
-#   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
-
+t1<- tabla[1:(nrow(tabla)-1),] # le quitamos la fila con el total
 
 
 # intento 1
-ggplot(data = tabla) +
-  geom_bar(mapping = aes(x = Comuna, fill = Fallecidos)) +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+# ggplot(data = tabla) +
+#   geom_bar(mapping = aes(x = Comuna, fill = Fallecidos)) +
+#   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
 
 #intento 2
-ggplot(data = tabla) +
-  geom_bar(mapping = aes(x = Comuna, fill = Confirmados), position = "fill") +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
-
-tabla
+# ggplot(data = tabla) +
+#   geom_bar(mapping = aes(x = Comuna, fill = Confirmados), position = "fill") +
+#   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
 
 # intento 3
-ggplot(data = tabla, aes(x = Comuna, y = Confirmados, fill = Fallecidos)) +
-  geom_bar(position = "stack", stat = "identity") +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+# ggplot(data = tabla, aes(x = Comuna, y = Confirmados, fill = Fallecidos)) +
+#   geom_bar(position = "stack", stat = "identity") +
+#   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
 
 # intento 4
-t1<- tabla[1:(nrow(tabla)-1),] # le quitamos la fila con el total
 
-ggplot(data=t1, aes(x = Comuna, y = Confirmados, fill = Fallecidos)) +
-  geom_bar(stat = "identity")+
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+# ggplot(data=t1, aes(x = Comuna, y = Confirmados, fill = Fallecidos)) +
+#   geom_bar(stat = "identity")+
+#   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
 
 # internto 5 $$$$$
-ggplot(data = t1, aes(x = Comuna, y = Confirmados, color = as.factor(Fallecidos))) +
-  geom_bar(stat = "identity", position = "dodge") +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+# ggplot(data = t1, aes(x = Comuna, y = Confirmados, color = as.factor(Fallecidos))) +
+#   geom_bar(stat = "identity", position = "dodge") +
+#   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
 
 #t.tabla2 <- as.data.frame(t.tabla)
 
 # intento 6 con facetas
-ggplot(data = t1, aes(x = Comuna, y = Confirmados, fill = Fallecidos)) +
-  geom_bar(position = "dodge", stat = "identity") +
-  facet_wrap(~Fallecidos)+
+# ggplot(data = t1, aes(x = Comuna, y = Confirmados, fill = Fallecidos)) +
+#   geom_bar(position = "dodge", stat = "identity") +
+#   facet_wrap(~Fallecidos) +
+#   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+
+# intento 7
+# t1.order <- t1[order(-t1$Confirmados),]
+# 
+# ggplot(mapping = aes(x, y)) +
+#   geom_bar(data = data.frame(x = t1.order$Comuna, y = t1.order$Confirmados), stat = "identity", fill = "yellow") +
+#   geom_bar(data = data.frame(x = t1.order$Comuna, y = t1.order$Fallecidos), stat = "identity", fill = "black") +
+#   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+
+
+# intento 8 para ordenar las columnas
+# ggplot(mapping = aes(x, y)) +
+#   geom_bar(data = data.frame(x = reorder(t1$Comuna, -t1$Confirmados), y = t1$Confirmados), stat = "identity", fill = "yellow") +
+#   geom_bar(data = data.frame(x = reorder(t1$Comuna, -t1$Confirmados), y = t1$Fallecidos), stat = "identity", fill = "black") +
+#   labs(title = "Casos por comuna", subtitle = "Región del Maule", x = "Comuna", y = "Nro. de casos") +
+#   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+
+# intento 9 agregando leyenda
+ggplot(mapping = aes(x, y)) +
+  geom_bar(data = data.frame(x = reorder(t1$Comuna, -t1$Confirmados), y = t1$Confirmados), stat = "identity", fill = "yellow") +
+  geom_bar(data = data.frame(x = reorder(t1$Comuna, -t1$Confirmados), y = t1$Fallecidos), stat = "identity", fill = "black") +
+  scale_fill_manual("Casos", values = c("Confirmados" = "yellow", "Fallecidos" = "black"), aesthetics = "fill") +
+  labs(title = "Casos por comuna", subtitle = "Región del Maule", x = "Comuna", y = "Nro. de casos") +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+
 
 # ggplot ####################################
 ggplot(data = t1, aes(x = Comuna, y = Confirmados, color = Fallecidos)) +
